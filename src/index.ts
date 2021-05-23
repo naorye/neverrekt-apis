f859f60c147ad53193d8c2df9afe6cbc54a0fc83
Naor Ye <naor.yehudaey.naturalint.com>
2021-05-23 04:36:37 +0300

    - Add test code

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
 export { Kucoin } from "./Kucoin";
 export { Gateio } from "./Gateio";
+
+// import { Gateio } from "./Gateio";
+
+// const api = new Gateio();
+
+// api.getTickerInformation().then((a) => console.log(a));
